b0e61a2a437b3a3d1f69e6c4f9abb747d86c5a0b
David Jetsupphasuk <jetsdavid4@gmail.com>
2023-02-05 23:42:53 -0800

    created login react component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,16 @@
-import React from 'react';
-
+import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
+import Login from "./components/auth/Login";
+import Protected from "./components/auth/Protected";
 
 function App() {
   return (
     <div className="App">
-     HELLO WORLD
+      <BrowserRouter>
+        <Routes>
+          <Route path='/login' element={<Login />}/>
+          <Route path='/protected' element={<Protected />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
