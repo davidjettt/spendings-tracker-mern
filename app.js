720f427452540f07e184ef8083912ca71a303cc7
David Jetsupphasuk <jetsdavid4@gmail.com>
2023-03-11 00:33:18 -0800

    added types to charts and columnChart components; added comments to app.js file

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const passport = require('passport')
 const cors = require('cors')
 const app = express()
 
+// goes to build directory when in production
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('frontend/build'));
   app.get('/', (req, res) => {
@@ -32,17 +33,17 @@ if (process.env.NODE_ENV === 'production') {
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// using mongoose to connect to mongoDB
 mongoose
   .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
 
-
-// app.get("/", (req, res) => res.send("Hello World"));
-
+// initializes passport
 app.use(passport.initialize())
 require('./config/passport')
 
+// connects API routes to app
 app.use('/api/transactions', transactions)
 app.use('/api/users', users)
 app.use('/api/auth', auth)
@@ -52,6 +53,3 @@ const PORT = process.env.PORT || 8001
 app.listen(PORT, () => {
 console.log(`Listening on port: ${PORT}`)
 })
-// http.listen(PORT, () => {
-//   console.log('Listening on port ' + PORT)
-// })
